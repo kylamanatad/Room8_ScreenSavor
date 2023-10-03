5643083d9eabd377bd977facfbd9fde452d1c3fa
tarshibel <taraysbel.datan@cit.edu>
2023-10-03 17:30:35 +0800

    Changes by Charlotte

--- review/models.py
+++ review/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import User
+from movie.models import Movie
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
+    rating = models.PositiveIntegerField()
+    description = models.TextField()
+
