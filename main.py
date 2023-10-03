1e38ab6f132aa436b300de13dfb68e4a4343a19e
tarshibel <taraysbel.datan@cit.edu>
2023-10-03 14:45:55 +0800

    django & sql installed, database created

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+# Todo
+# 1. Create Django Project (project name: Room8_ScreenSavor)
+# 2. Change Database to MySQL
+# 'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'screensavor',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {'init_command': "SET SQL_MODE='STRICT_TRANS_TABLES'"}
+# 3. Install mysqlclient (done)
+# - Create Database locally
+# 4. Migrate: py manage.py migrate
+# 5. Create Super User: py manage.py createsuperuser
+# 6. Create 5 apps: py manage.py startapp <<app name>>
+# 7. Add Models (On the apps)
+# 8. Migrate
+# 9. Add Apps to settings.py
+# 10. Submit
